b93b3d9ea0c47c1421cb276a527c86f27d8f857c
uwen-godwin <uwengodwin001@gmail.com>
2024-01-30 13:55:00 -0800

    0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/env ruby
-regex = /\[(from:|to:)(\+?[a-zA-Z0-9]+)\].* \[flags:([^]]+)\]/
-matches = ARGV[0].scan(regex)
-puts matches.map { |match| match[1] + "," + match[2] + "," + match[3] }.join("\n")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
