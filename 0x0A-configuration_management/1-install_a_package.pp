d3f397d79b59688206886aef7a3fe11ba4158ad5
uwen-godwin <uwengodwin001@gmail.com>
2024-03-23 07:52:10 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,10 @@
 # Puppet manifest to install Flask from pip3
-package { 'FLASKg':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+# Puppet manifest to install Werkzeug from pip3
 package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
